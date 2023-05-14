cb0cb3601dba472937ea44202a1664c21c4d2075
Cheon Jaewon <navistwon97@gmail.com>
2023-05-14 07:01:55 +0000

    feat/add_only_punct 완성
    - 작동 확인 필요
    - 다른 add method들과 같이 쓰지 말것
    -short_tokenizing과 함께 쓸 것

--- utils/data_controller.py
+++ utils/data_controller.py
@@ -391,24 +391,27 @@ class DataCleaning():
                 
         Ex)
         From ->     〈Something〉는 조지 해리슨이 쓰고 비틀즈가 1969년 앨범 《Abbey Road》에 담은 노래다.
-        To ->       〈Something〉는 # ^ PER ^ 조지 해리슨 #이 쓰고 @ * ORG * 비틀즈 @가 1969년 앨범 《Abbey Road》에 담은 노래다.
+        To ->       〈Something〉는 # ^ PER ^ 조지 해리슨 # 이 쓰고 @ * ORG * 비틀즈 @ 가 1969년 앨범 《Abbey Road》에 담은 노래다.
         """
-        # [{S|O}:{type}] 태크 달아주기
+        
         new_sentence = []
         for _, row in df.iterrows():
             sentence = row["sentence"]
+            sub_type = '* ' + row["sub_type"] + ' * '
+            obj_type = '^ ' + row["obj_type"] + ' ^ '
+            
             trigger = True if row['object_end_idx'] > row['subject_end_idx'] else False
 
             for check, idx in enumerate(sorted([row['subject_start_idx'], row['subject_end_idx'], row['object_start_idx'], row['object_end_idx']], reverse=True)):
                 if trigger:
-                    token = f"O:{row['object_type']}"
+                    token = " # "
                 else:
-                    token = f"S:{row['subject_type']}"
+                    token = " @ "
 
                 if check % 2 == 0:
-                    sentence = sentence[:idx+1] + f" [/{token}] " + sentence[idx+1:]
+                    sentence = sentence[:idx+1] + token + sentence[idx+1:]
                 else:
-                    sentence = sentence[:idx] + f"[{token}] " + sentence[idx:]
+                    sentence = sentence[:idx] + obj_type if trigger else sub_type + token  + sentence[idx:]
                     trigger = not trigger
             
             new_sentence.append(sentence)
